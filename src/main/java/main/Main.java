67f1f6fb7dca8d986ead8a773fa270fc7b79627c
Mohammad reza saeidi <09130088640m@gmail.com>
2022-09-27 11:15:47 +0330

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,21 +18,5 @@ public class Main {
         } catch (PcapNativeException | UnknownHostException e) {
             e.printStackTrace();
         }
-
-//        try {
-//            PcapNetworkInterface networkInterface = Pcaps.findAllDevs().get(0);
-//            ArpPacket arpPacket = new ArpPacket.Builder()
-//                    .srcProtocolAddr(networkInterface.getAddresses().get(0).getAddress())
-//                    .srcHardwareAddr(MacAddress.getByAddress(networkInterface.getLinkLayerAddresses().get(0).getAddress()))
-//                    .dstProtocolAddr(InetAddress.getByName("192.168.1.1"))
-//                    .dstHardwareAddr(MacAddress.ETHER_BROADCAST_ADDRESS)
-//                    .hardwareType(ArpHardwareType.ETHERNET)
-//                    .operation(ArpOperation.REQUEST)
-//                    .protocolType(EtherType.IPV4)
-//                    .build();
-//            new PcapHandle.Builder(networkInterface.getName()).build().sendPacket(arpPacket);
-//        } catch (PcapNativeException | UnknownHostException | NotOpenException e) {
-//            e.printStackTrace();
-//        }
     }
 }
